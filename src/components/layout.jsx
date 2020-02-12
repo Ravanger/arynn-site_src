af7590e30ecd15919f994a15362771bbb1de32d1
Ravanger <Boris.Rozzy@gmail.com>
2020-02-12 01:28:00 -0500

    css refactor

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header/Header"
 
-import "./layout.css"
+import "./base.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
