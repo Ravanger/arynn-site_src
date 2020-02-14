18cca4ed9311ed99bf13851223ec9adb80d7ae86
Ravanger <Boris.Rozzy@gmail.com>
2020-02-14 16:03:26 -0500

    Removed page-2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,7 +9,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <PortfolioImages />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
