3c5dfffefb5692d2c7d634020d7167f334f7fb43
Ravanger <Boris.Rozzy@gmail.com>
2020-02-15 18:57:03 -0500

    Hover submenu; extended MainMenuItem functionality

--- src/components/Header/MainMenu/MainMenu.jsx
+++ src/components/Header/MainMenu/MainMenu.jsx
@@ -5,17 +5,29 @@ import MainMenuItem from "./MainMenuItem/MainMenuItem"
 
 import styles from "./MainMenu.module.css"
 
+const mobileLayoutBreak = "35.5em"
+
 const MainMenu = () => (
   <ResponsiveMenu
     menuOpenButton={"X"}
     menuCloseButton={"X"}
-    changeMenuOn="568px"
+    changeMenuOn={mobileLayoutBreak}
     menu={
       <div className={"pure-g " + styles.center}>
         <ul className={"pure-u-1-1 " + styles.mainMenu}>
-          <MainMenuItem title="work" to="/" />
+          <MainMenuItem title="work" to="/" hoverClass={styles.hoverClass}>
+            <ul class={styles.dropdownContent}>
+              <MainMenuItem title="acrylic paintings" to="/" isSubMenu />
+              <MainMenuItem title="ink illustrations" to="/" isSubMenu />
+              <MainMenuItem title="digital artwork" to="/" isSubMenu />
+            </ul>
+          </MainMenuItem>
           <MainMenuItem title="about me" to="/" />
-          <MainMenuItem title="patreon" to="/" />
+          <MainMenuItem
+            title="patreon"
+            to="https://www.patreon.com/artsyarynn"
+            isExternal
+          />
         </ul>
       </div>
     }
--- src/components/Header/MainMenu/MainMenuItem/MainMenuItem.jsx
+++ src/components/Header/MainMenu/MainMenuItem/MainMenuItem.jsx
@@ -2,17 +2,58 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const MainMenuItem = ({ title, to }) => (
-  <li className="pure-u-1 pure-u-sm-1-3">
-    <Link to={to} activeClassName="active">
+import "./MainMenuItem.module.css"
+
+const MainMenuItem = ({
+  children,
+  title,
+  to,
+  hoverClass,
+  isSubMenu,
+  isExternal,
+}) => (
+  <li
+    className={"pure-u-1 " + hoverClass + (isSubMenu ? "" : " pure-u-sm-1-3")}
+  >
+    {/* <Link to={to} activeClassName="active">
       {title}
-    </Link>
+    </Link> */}
+    {(() => {
+      if (isExternal) {
+        return (
+          <a
+            href={to}
+            target="_blank"
+            rel="noopener noreferrer"
+            activeClassName="active"
+          >
+            {title}
+          </a>
+        )
+      } else {
+        return (
+          <Link to={to} activeClassName="active">
+            {title}
+          </Link>
+        )
+      }
+    })()}
+    {children}
   </li>
 )
+
+MainMenuItem.defaultProps = {
+  hoverClass: ``,
+  isSubMenu: false,
+  isExternal: false,
+}
 
 MainMenuItem.propTypes = {
   title: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
+  hoverClass: PropTypes.string,
+  isSubMenu: PropTypes.bool,
+  isExternal: PropTypes.bool,
 }
 
 export default MainMenuItem
