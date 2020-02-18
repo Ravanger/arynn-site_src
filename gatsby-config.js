aad0698849088b3a1f01f17f08cd68bcb896e680
Ravanger <Boris.Rozzy@gmail.com>
2020-02-17 20:29:18 -0500

    json

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
         path: `${__dirname}/src/style/fonts`,
       },
     },
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
