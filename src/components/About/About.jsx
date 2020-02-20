31dc2cfc38d5a51c4756fa450fe8817f1cc11c67
Ravanger <Boris.Rozzy@gmail.com>
2020-02-19 19:32:37 -0500

    About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { useStaticQuery, graphql } from "gatsby"
+import GatsbyImage from "gatsby-image"
 
 import SocialsMenu from "../SocialsMenu/SocialsMenu"
 
@@ -16,17 +17,23 @@ const About = () => {
           about
         }
       }
+      file(sourceInstanceName: { eq: "images" }, name: { eq: "about" }) {
+        childImageSharp {
+          fluid(quality: 70, maxWidth: 915) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
     }
   `)
+
   return (
     <div className={"pure-g " + styles.wrapper}>
-      {/* TODO: replace with image */}
       <div className={"pure-u-2 " + styles.aboutContainer}>
-        <h2 className={styles.name}>{data.site.siteMetadata.title}</h2>
-        <p className={styles.description}>
-          {data.site.siteMetadata.description}
-        </p>
-        <p className={styles.aboutblurb}>{data.site.siteMetadata.about}</p>
+        <GatsbyImage
+          fluid={data.file.childImageSharp.fluid}
+          className={styles.avatar}
+        />
       </div>
       <div className={"pure-u-2 " + styles.aboutContainer}>
         <h2 className={styles.name}>{data.site.siteMetadata.title}</h2>
