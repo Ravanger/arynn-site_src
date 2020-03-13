ea4945919d44f4428ae100d93094074f2ed200fd
Ravanger <Boris.Rozzy@gmail.com>
2020-03-13 19:27:56 -0400

    Move css imports; styling

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,13 @@
+import "./src/style/base.css"
+import "purecss/build/base-min.css"
+import "purecss/build/grids-min.css"
+import "purecss/build/grids-responsive-min.css"
+
+import "@reach/dialog/styles.css"
+import "./src/style/style.css"
+import "./src/style/fonts.css"
+import "./src/style/colours.css"
+
 export const onServiceWorkerUpdateReady = () => {
   window.location.reload(true)
 }
