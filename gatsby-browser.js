dcbf8f2d319a6efa35f6aa033fa18b27aadb1ca6
Boris Rossovsky <Boris.Rozzy@gmail.com>
2021-01-18 15:03:46 -0500

    Switch to nextjs - new project

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-import "./src/static/style/reset.css"
-import "purecss/build/grids-min.css"
-import "purecss/build/grids-responsive-min.css"
-import "@reach/dialog/styles.css"
-
-export const onServiceWorkerUpdateReady = () => {
-  window.location.reload(true)
-}
