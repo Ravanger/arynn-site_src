71c004dc3cfea7b933232408084275c141b92f59
Boris Rossovsky <Boris.Rozzy@gmail.com>
2021-01-20 19:50:54 -0500

    Clean up global reset css

--- styles/global.ts
+++ styles/global.ts
@@ -10,7 +10,6 @@ const reset = css`
   body,
   div,
   span,
-  applet,
   object,
   iframe,
   h1,
@@ -20,38 +19,17 @@ const reset = css`
   h5,
   h6,
   p,
-  blockquote,
-  pre,
   a,
-  abbr,
-  acronym,
-  address,
-  big,
-  cite,
-  code,
-  del,
-  dfn,
   em,
   img,
-  ins,
-  kbd,
-  q,
-  s,
-  samp,
-  small,
   strike,
   strong,
   sub,
   sup,
-  tt,
-  var,
   b,
   u,
   i,
   center,
-  dl,
-  dt,
-  dd,
   ol,
   ul,
   li,
@@ -59,32 +37,20 @@ const reset = css`
   form,
   label,
   legend,
-  table,
   caption,
-  tbody,
-  tfoot,
-  thead,
-  tr,
-  th,
-  td,
   article,
   aside,
-  canvas,
   details,
   embed,
   figure,
   figcaption,
   footer,
   header,
-  hgroup,
   menu,
   nav,
-  output,
-  ruby,
   section,
   summary,
   time,
-  mark,
   audio,
   video {
     margin: 0;
@@ -93,6 +59,7 @@ const reset = css`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
+    box-sizing: border-box;
   }
   /* HTML5 display-role reset for older browsers */
   article,
@@ -102,7 +69,6 @@ const reset = css`
   figure,
   footer,
   header,
-  hgroup,
   menu,
   nav,
   section {
@@ -115,26 +81,8 @@ const reset = css`
   ul {
     list-style: none;
   }
-  blockquote,
-  q {
-    quotes: none;
-  }
-  blockquote:before,
-  blockquote:after,
-  q:before,
-  q:after {
-    content: "";
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
   a {
     text-decoration: none;
-  }
-  * {
-    box-sizing: border-box;
   }
 `
 
