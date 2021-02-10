f1ebf6ffa41d7d4642a183a42c51a3b53f942ce8
Boris Rossovsky <Boris.Rozzy@gmail.com>
2021-02-10 00:30:48 -0500

    Responsive shop grid

--- src/ShopPage/ShopItems/ShopItems.styles.ts
+++ src/ShopPage/ShopItems/ShopItems.styles.ts
@@ -3,6 +3,14 @@ import styled from "styled-components"
 export const DivShopItems = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(1, 1fr);
   grid-gap: 2rem;
+
+  @media (min-width: 42rem) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 60rem) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `
