8fe006351487d23e06569fbe5fb9e2cc247aab86
Ravanger <Boris.Rozzy@gmail.com>
2021-02-13 22:57:06 -0500

    Unnecessary API endpoint

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,8 +1,8 @@
 import ShopPage from "src/ShopPage"
 import { getLayout } from "src/layouts/MainLayout/MainLayout"
 import SEO from "src/common/SEO"
-import { getShopItems } from "./api/shopitems"
 import { InferGetStaticPropsType } from "next"
+import { ShopItem } from "src/ShopPage/ShopPage.types"
 
 export const Shop = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
@@ -14,10 +14,84 @@ export const Shop = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
 }
 
 export const getStaticProps = async () => {
-  const shopItems = await getShopItems()
+  const shopItems: ShopItem[] = [
+    {
+      title: "Custom portrait",
+      image: "http://placekitten.com/500/500",
+      price: "35+",
+      type: "Custom",
+    },
+    {
+      title: "Sticker package",
+      image: "http://placekitten.com/400/400",
+      price: "8",
+      type: "Stickers",
+    },
+    {
+      title: "Original Painting Title",
+      image: "http://placekitten.com/450/450",
+      price: "300",
+      type: "Originals",
+    },
+    {
+      title: "Custom portrait",
+      image: "http://placekitten.com/600/600",
+      price: "35+",
+      type: "Custom",
+    },
+    {
+      title: "Sticker package",
+      image: "http://placekitten.com/550/333",
+      price: "8",
+      type: "Stickers",
+    },
+    {
+      title: "Original Painting Title",
+      image: "http://placekitten.com/375/375",
+      price: "300",
+      type: "Artwork",
+    },
+    {
+      title: "Custom portrait",
+      image: "http://placekitten.com/425/300",
+      price: "35+",
+      type: "Custom",
+    },
+    {
+      title: "Sticker package",
+      image: "http://placekitten.com/475/475",
+      price: "8",
+      type: "Stickers",
+    },
+    {
+      title: "Original Painting Title",
+      image: "http://placekitten.com/525/500",
+      price: "300",
+      type: "Originals",
+    },
+    {
+      title: "Custom portrait",
+      image: "http://placekitten.com/575/500",
+      price: "35+",
+      type: "Custom",
+    },
+    {
+      title: "Sticker package",
+      image: "http://placekitten.com/350/500",
+      price: "8",
+      type: "Stickers",
+    },
+    {
+      title: "Original Painting Title",
+      image: "http://placekitten.com/666/666",
+      price: "300",
+      type: "Artwork",
+    },
+  ]
 
   return {
     props: { shopItems },
+    revalidate: 600,
   }
 }
 
--- src/ShopPage/ShopPage.types.ts
+++ src/ShopPage/ShopPage.types.ts
@@ -1,4 +1,9 @@
-import { ShopItem } from "pages/api/shopitems"
+export interface ShopItem {
+  title: string
+  image: string
+  price: string
+  type: string
+}
 
 export interface ShopPageType {
   shopItems: ShopItem[]
