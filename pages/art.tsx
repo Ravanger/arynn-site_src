7f2f62cd2ca712863c85567d42276a2ebf59deda
Ravanger <Boris.Rozzy@gmail.com>
2021-02-14 03:36:16 -0500

    Clear art filter atom on useEffect

--- pages/art.tsx
+++ pages/art.tsx
@@ -3,8 +3,16 @@ import SEO from "src/common/SEO"
 import { getLayout } from "src/layouts/MainLayout/MainLayout"
 import { GetStaticProps, InferGetStaticPropsType } from "next"
 import { ART_ITEMS_MOCK_DATA } from "util/sampleData"
+import { useEffect } from "react"
+import { useAtom } from "jotai"
+import { artFilterAtom } from "atoms/store"
 
 const Art = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [, setArtFilter] = useAtom(artFilterAtom)
+
+  useEffect(() => {
+    setArtFilter("")
+  }, [])
   return (
     <>
       <SEO title="Art" description="Arynn's art" url="/art" />
