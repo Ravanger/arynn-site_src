6065eb41d72bc562d7064f4d1960ff3b7b25f9a8
Ravanger <Boris.Rozzy@gmail.com>
2021-03-10 01:44:34 -0500

    misc

--- src/common/MainImage/MainImage.tsx
+++ src/common/MainImage/MainImage.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image"
 
+const src = "/images/new_self_portrait.svg"
+
 const MainImage = () => (
   <Image
-    src="/images/new_self_portrait.svg"
+    src={src}
     alt="Arynn self portrait"
     quality={60}
     layout="fixed"
