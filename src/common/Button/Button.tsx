99a37c40074e317ec974012db37acd2f3823d82d
Ravanger <Boris.Rozzy@gmail.com>
2021-03-10 23:19:45 -0500

    Add onClick prop to button

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -9,6 +9,7 @@ const Button = (props: ButtonProps) => {
   return (
     <button
       type={props.type}
+      onClick={props.onClick}
       className={`font-bold rounded-lg p-4 w-full self-end bg-pink hover:bg-blue active:bg-blue-light text-white text-xl ${props.className}`}
     >
       {props.children}
--- src/common/Button/Button.types.ts
+++ src/common/Button/Button.types.ts
@@ -1,5 +1,8 @@
+import { MouseEventHandler } from "react"
+
 export interface ButtonProps {
   type?: "button" | "submit" | "reset"
+  onClick?: MouseEventHandler<HTMLButtonElement>
   children: React.ReactNode
   className?: string
 }
