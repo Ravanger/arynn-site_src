9768d35c8515dc0dc592ea3e86636d2a75a6c902
Ravanger <Boris.Rozzy@gmail.com>
2021-03-31 03:17:25 -0400

    Cart item layout

--- src/CartPage/CartItem/CartItem.tsx
+++ src/CartPage/CartItem/CartItem.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image"
+import { SplitAndCapitalizeFirstWord } from "util/text"
 import { CartItemType } from "./CartItem.types"
 
 const CartItem = (props: CartItemType) => (
-  <div>
-    <h2 className="inline">{props.item.title}</h2>
-    <button onClick={props.removeCartItem}>Remove</button>
+  <div className="grid grid-cols-1 md:grid-cols-2">
+    <Image
+      src={props.item.image}
+      alt={props.item.title}
+      layout="intrinsic"
+      width={600}
+      height={600}
+      objectFit="cover"
+    />
+    <div>
+      <button onClick={props.removeCartItem}>Remove</button>
+      <h2>{props.item.title}</h2>
+      <h3>{SplitAndCapitalizeFirstWord(props.item.type, "_")}</h3>
+      <p>{props.item.description}</p>
+      <span>${props.item.price}</span>
+    </div>
   </div>
 )
 
