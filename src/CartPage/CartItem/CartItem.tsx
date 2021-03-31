c9dc305edda94f89bad9628c91c5bdd20f41cf02
Ravanger <Boris.Rozzy@gmail.com>
2021-03-31 04:08:54 -0400

    Cart page styling

--- src/CartPage/CartItem/CartItem.tsx
+++ src/CartPage/CartItem/CartItem.tsx
@@ -1,24 +1,30 @@
 import Image from "next/image"
+import HeaderBar from "src/common/HeaderBar"
 import { SplitAndCapitalizeFirstWord } from "util/text"
 import { CartItemType } from "./CartItem.types"
+import { IoClose } from "react-icons/io5"
 
 const CartItem = (props: CartItemType) => (
-  <div className="grid grid-cols-1 md:grid-cols-2">
-    <Image
-      src={props.item.image}
-      alt={props.item.title}
-      layout="intrinsic"
-      width={600}
-      height={600}
-      objectFit="cover"
-    />
-    <div>
-      <button onClick={props.removeCartItem}>Remove</button>
-      <h2>{props.item.title}</h2>
-      <h3>{SplitAndCapitalizeFirstWord(props.item.type, "_")}</h3>
-      <p>{props.item.description}</p>
-      <span>${props.item.price}</span>
+  <div className="grid  gap-y-4 md:gap-x-4 md:grid-cols-2 md:grid-rows-2">
+    <div className="w-full md:row-span-2 md:self-center">
+      <Image
+        src={props.item.image}
+        alt={props.item.title}
+        layout="responsive"
+        width={600}
+        height={600}
+        objectFit="cover"
+      />
     </div>
+    <div className="relative">
+      <button onClick={props.removeCartItem} className="absolute right-0 top-0">
+        <IoClose />
+      </button>
+      <HeaderBar>{props.item.title}</HeaderBar>
+      <h3>{SplitAndCapitalizeFirstWord(props.item.type, "_")}</h3>
+      <p className="text-justify">{props.item.description}</p>
+    </div>
+    <span className="md:col-start-2">${props.item.price}</span>
   </div>
 )
 
--- src/CartPage/CartPage.tsx
+++ src/CartPage/CartPage.tsx
@@ -1,19 +1,23 @@
 import { shopCartAtom } from "atoms/store"
 import { useAtom } from "jotai"
+import HeaderBar from "src/common/HeaderBar"
 import CartItem from "./CartItem"
 
 const CartPage = () => {
   const [cartItems, dispatchCartAction] = useAtom(shopCartAtom)
 
   return (
-    <>
+    <div className="space-y-4">
       {cartItems.map((cartItem) => (
-        <CartItem
-          item={cartItem}
-          removeCartItem={() => {
-            dispatchCartAction({ type: "REMOVE", payload: cartItem })
-          }}
-        />
+        <>
+          <CartItem
+            item={cartItem}
+            removeCartItem={() => {
+              dispatchCartAction({ type: "REMOVE", payload: cartItem })
+            }}
+          />
+          <HeaderBar />
+        </>
       ))}
       <button
         onClick={() => {
@@ -22,7 +26,7 @@ const CartPage = () => {
       >
         Clear cart
       </button>
-    </>
+    </div>
   )
 }
 
