4b1fef619e4bb8b00ffc46bf5acb5a0d411ae4f4
Ravanger <Boris.Rozzy@gmail.com>
2021-04-19 16:09:36 -0400

    Error 404 text pink

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -6,7 +6,7 @@ const Success = () => {
   return (
     <>
       <SEO title="404" description="Thank you for your order" url="/success" />
-      <h2 className="text-4xl font-bold">Error 404</h2>
+      <h2 className="text-4xl font-bold text-pink">Error 404</h2>
       <Spacer size="2rem" />
       <p className="font-bold italic">What are you doing?</p>
       <p className="italic">There's nothing to see here.</p>
