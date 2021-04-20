596c653f94fc55a4d1e4d653559a8ae6d00e887b
Ravanger <Boris.Rozzy@gmail.com>
2021-04-19 20:47:07 -0400

    Landing page menu spacing fix

--- src/HomePage/Menu/Menu.tsx
+++ src/HomePage/Menu/Menu.tsx
@@ -6,13 +6,13 @@ import { IoTriangle } from "react-icons/io5"
 
 const Menu = (props: MenuPropsType) => {
   const menu = props.menuItems.map((item) => {
-    const isButtonTop = item.text === "Shop"
+    const isButtonShop = item.text === "Shop"
     return (
       <React.Fragment key={item.text}>
         <Spacer size="0.5rem" />
         <div
           className={`animate-scaleExpandIn hover:animate-scaleExpandOut text-center lg:relative z-10 ${
-            isButtonTop ? "lg:top-15p" : "lg:top-1/3"
+            isButtonShop ? "lg:top-15p" : "lg:top-1/3"
           }`}
         >
           <Button text={item.text} url={item.url} />
@@ -27,7 +27,7 @@ const Menu = (props: MenuPropsType) => {
   })
 
   return (
-    <nav className="flex flex-nowrap justify-around items-center w-full flex-col h-2/3 lg:h-screen lg:flex-row lg:items-baseline">
+    <nav className="flex flex-nowrap justify-around items-center w-full flex-col lg:h-screen lg:flex-row lg:items-baseline">
       {menu}
     </nav>
   )
