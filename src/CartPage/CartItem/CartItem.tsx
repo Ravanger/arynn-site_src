1005bb42b54020d29da306fba316c35ad5932cfb
Ravanger <Boris.Rozzy@gmail.com>
2021-04-29 00:14:05 -0400

    Go to shop item page from cart

--- src/CartPage/CartItem/CartItem.tsx
+++ src/CartPage/CartItem/CartItem.tsx
@@ -5,6 +5,7 @@ import { FaWindowClose } from "react-icons/fa"
 import Spacer from "src/common/Spacer"
 import { CURRENCY } from "util/stripe"
 import { formatCurrencyString } from "use-shopping-cart"
+import Link from "next/link"
 
 const CartItem = (props: CartItemType) => (
   <div className="rounded-xl p-10 relative grid gap-8 md:gap-16 md:grid-cols-3 md:items-center sm:border-15 sm:border-blue">
@@ -17,23 +18,29 @@ const CartItem = (props: CartItemType) => (
         className="text-pink hover:text-blue active:text-blue-light"
       />
     </button>
-    <div className="w-full self-center">
-      <Image
-        src={props.item.image!}
-        alt={props.item.name}
-        layout="responsive"
-        width={600}
-        height={600}
-        objectFit="cover"
-      />
-    </div>
-    <div className="italic md:text-left">
-      <h3 className="font-bold sm:text-2xl">{props.item.name}</h3>
-      <Spacer />
-      <p className="md:text-justify text-xs sm:text-sm">
-        {props.item.description}
-      </p>
-    </div>
+    <Link href={`/shop/${props.item.sku}`}>
+      <a className="group">
+        <div className="w-full self-center">
+          <Image
+            src={props.item.image!}
+            alt={props.item.name}
+            layout="responsive"
+            width={600}
+            height={600}
+            objectFit="cover"
+          />
+        </div>
+        <div className="italic md:text-left">
+          <h3 className="font-bold group-hover:text-pink sm:text-2xl">
+            {props.item.name}
+          </h3>
+          <Spacer />
+          <p className="text-xs sm:text-sm md:text-justify">
+            {props.item.description}
+          </p>
+        </div>
+      </a>
+    </Link>
     <div className="flex flex-row items-center gap-4 justify-center">
       <HeaderBar hrClassName="hidden flex-grow flex-shrink bg-blue h-0.5 border-none lg:block" />
       <span className="text-3xl">
