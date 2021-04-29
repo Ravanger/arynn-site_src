1c58f9b29ab5a1323716907949178208d1c0d11b
Ravanger <Boris.Rozzy@gmail.com>
2021-04-29 14:12:29 -0400

    z-index bug

--- src/common/ContentBox/ContentBox.tsx
+++ src/common/ContentBox/ContentBox.tsx
@@ -2,7 +2,7 @@ import { ContentBoxProps } from "./ContentBox.types"
 
 const ContentBox = (props: ContentBoxProps) => {
   return (
-    <div className="max-w-5xl w-full bg-white text-center overflow-hidden shadow-md py-8 px-4 sm:rounded-xl4 sm:p-16">
+    <div className="max-w-5xl w-full bg-white text-center overflow-hidden shadow-md py-8 px-4 relative z-10 sm:rounded-xl4 sm:p-16">
       {props.children}
     </div>
   )
