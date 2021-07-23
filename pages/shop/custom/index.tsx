a38e2b990d738641ff0c804015f367db384fc57d
Ravanger <Boris.Rozzy@gmail.com>
2021-07-22 23:25:35 -0400

    useMemo for totalPrice

--- pages/shop/custom/index.tsx
+++ pages/shop/custom/index.tsx
@@ -4,7 +4,7 @@ import CustomShopPage from "src/ShopPage/CustomShopPage"
 import { GetStaticProps, InferGetStaticPropsType } from "next"
 import { getShopItems } from "util/dataFetching"
 import { readFile, writeFile } from "util/cache"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 import { SelectedCustomAddons } from "src/ShopPage/CustomShopPage/CustomShopPage.types"
 import { useAtom } from "jotai"
 import { cartAtom } from "atoms/store"
@@ -13,26 +13,11 @@ import { CustomProductType } from "util/data.types"
 
 const Custom = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   const [cartItems, setCartItems] = useAtom(cartAtom)
-  const [totalPrice, setTotalPrice] = useState<number>(
-    props.customShopInfo.price
-  )
   const [quantityInCart, setQuantityInCart] = useState(0)
   const [selectedCustomAddons, setSelectedCustomAddons] =
     useState<SelectedCustomAddons>({
       ...props.customShopInfo.customData.selectedAddons,
     })
-
-  // Update total price whenever addons change
-  useEffect(() => {
-    const addonTotalPrice =
-      selectedCustomAddons.type.price +
-      selectedCustomAddons.numberOfPeople.price +
-      selectedCustomAddons.addons.reduce(
-        (total, addonItem) => total + addonItem.price,
-        0
-      )
-    setTotalPrice(props.customShopInfo.price + addonTotalPrice)
-  }, [props.customShopInfo, selectedCustomAddons])
 
   // Update quantity in cart
   useEffect(() => {
@@ -40,6 +25,18 @@ const Custom = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
       getCustomProductQuantityInCart(cartItems, props.customShopInfo)
     )
   }, [cartItems, props.customShopInfo])
+
+  // Update total price whenever addons change
+  const totalPrice = useMemo(() => {
+    const addonTotalPrice =
+      selectedCustomAddons.type.price +
+      selectedCustomAddons.numberOfPeople.price +
+      selectedCustomAddons.addons.reduce(
+        (total, addonItem) => total + addonItem.price,
+        0
+      )
+    return props.customShopInfo.price + addonTotalPrice
+  }, [props.customShopInfo, selectedCustomAddons])
 
   if (props.errors || !props.customShopInfo) return <></>
 
