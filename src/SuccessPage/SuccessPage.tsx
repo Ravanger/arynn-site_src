47336b78bbd43cc0b8f7b0ef09a7027c8d30cb87
Ravanger <Boris.Rozzy@gmail.com>
2021-07-27 15:53:39 -0400

    Minor style tweaks and whatnot

--- src/SuccessPage/SuccessPage.tsx
+++ src/SuccessPage/SuccessPage.tsx
@@ -7,7 +7,9 @@ const SuccessPage = () => {
       <h2>Thank you for your order!</h2>
       <Spacer size="2rem" />
       <Link href="/shop">
-        <a className="text-pink hover:text-blue">Back to shop</a>
+        <a className="text-pink hover:text-blue active:text-blue-light">
+          Back to shop
+        </a>
       </Link>
     </>
   )
