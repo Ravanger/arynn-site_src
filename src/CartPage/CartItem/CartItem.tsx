3add5d2a309f357c16503ce5a55ee567c746b901
Ravanger <Boris.Rozzy@gmail.com>
2021-07-28 18:25:07 -0400

    minor typescript

--- src/CartPage/CartItem/CartItem.tsx
+++ src/CartPage/CartItem/CartItem.tsx
@@ -24,7 +24,7 @@ const CartItem = (props: CartItemType) => {
           value={props.quantityInCart}
           onChange={(event) => {
             props.setCartItems(
-              props.setWantedQuantity!(
+              props.setWantedQuantity(
                 props.cartItems,
                 props.item,
                 ~~event.target.value
