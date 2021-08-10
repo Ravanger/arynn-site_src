69e9789de3466bcc42de5232a4c3d553e1e42168
Ravanger <Boris.Rozzy@gmail.com>
2021-08-10 18:44:02 -0400

    Show what was ordered on success page

--- src/SuccessPage/SuccessPage.tsx
+++ src/SuccessPage/SuccessPage.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import Spacer from "src/common/Spacer"
+import { SuccessPagePropType } from "./SuccessPage.types"
 
-const SuccessPage = () => {
+const SuccessPage = (props: SuccessPagePropType) => {
   return (
     <>
       <h2>Thank you for your order!</h2>
       <Spacer size="2rem" />
+      <h3>Things ordered:</h3>
+      {props.orderedItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
       <Link href="/shop">
         <a className="text-pink hover:text-blue active:text-blue-light">
           Back to shop
