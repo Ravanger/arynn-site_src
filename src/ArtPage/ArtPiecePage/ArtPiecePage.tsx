fef7dfec4576c85706721e036ae4e6bde00d76ac
Ravanger <Boris.Rozzy@gmail.com>
2021-08-25 13:01:27 -0400

    Fix overlapping issue arrows

--- src/ArtPage/ArtPiecePage/ArtPiecePage.tsx
+++ src/ArtPage/ArtPiecePage/ArtPiecePage.tsx
@@ -11,14 +11,14 @@ import { motion } from "framer-motion"
 const ArtPiecePage = (props: ArtPiecePageProps) => {
   return (
     <>
-      <div className="fixed left-0 right-0 top-0 w-full h-full flex mx-auto items-center max-w-[68rem]">
+      <div className="fixed z-50 left-0 right-0 top-0 w-full h-full flex mx-auto items-center max-w-[68rem]">
         <ArtPrevNextButton
           type="PREV"
           href={{
             pathname: `/art/${props.prevItemId}`,
             ...(props.displayAll && { query: { display: "all" } }),
           }}
-          className="absolute left-0 sm:left-12"
+          className="-left-8 sm:left-4"
         />
         <ArtPrevNextButton
           type="NEXT"
@@ -26,7 +26,7 @@ const ArtPiecePage = (props: ArtPiecePageProps) => {
             pathname: `/art/${props.nextItemId}`,
             ...(props.displayAll && { query: { display: "all" } }),
           }}
-          className="absolute right-0 sm:right-12"
+          className="-right-8 sm:right-4"
         />
       </div>
       <HeaderBar>{SplitAndCapitalizeFirstWord(props.item.type, "_")}</HeaderBar>
@@ -60,7 +60,7 @@ const ArtPiecePage = (props: ArtPiecePageProps) => {
             <div className="flex flex-row w-full col-span-4 sm:col-span-2 sm:col-start-3">
               <Link href={props.item.shopItemUrl} passHref>
                 <motion.a
-                  className="font-bold rounded-lg p-4 w-full self-end bg-pink text-white text-lg md:text-xl select-none cursor-pointer block"
+                  className="font-bold rounded-lg p-4 mx-8 sm:mx-0 w-full self-end bg-pink text-white text-lg md:text-xl select-none cursor-pointer block relative z-50"
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                   transition={{ duration: 0.05 }}
